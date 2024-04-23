7a61f0b4eb38143b3aca64239a61bf440b137ef4
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-04-23 14:08:46 -0600

    dtos for orders done and added another service for order, already connected and working

--- src/products/entities/ProductsServicesNames.ts
+++ src/products/entities/ProductsServicesNames.ts
@@ -1,8 +1,9 @@
-export const PRODUCTS_SERVICES_NAMES = {
-  SERVICE_NAME: 'PRODUCT_SERVICE',
-  CREATE_PRODUCT: 'create_product',
-  FIND_ALL_PRODUCTS: 'find_all_products',
-  FIND_ONE_PRODUCT: 'find_one_product',
-  UPDATE_PRODUCT: 'update_product',
-  DELETE_PRODUCT: 'delete_product',
-};
+export enum PRODUCTS_SERVICES_NAMES {
+  SERVICE_NAME = 'PRODUCT_SERVICE',
+  CREATE_PRODUCT = 'create_product',
+  FIND_ALL_PRODUCTS = 'find_all_products',
+  FIND_ONE_PRODUCT = 'find_one_product',
+  UPDATE_PRODUCT = 'update_product',
+  DELETE_PRODUCT = 'delete_product',
+  VALIDATE_PRODUCTS = 'validate_products',
+}
