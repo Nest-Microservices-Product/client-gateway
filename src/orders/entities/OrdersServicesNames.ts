95c9b039e21bb67e54ebdc16160f63b319e2f0db
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-04-25 10:53:26 -0600

    update entity ordersServicesNames

--- src/orders/entities/OrdersServicesNames.ts
+++ src/orders/entities/OrdersServicesNames.ts
@@ -3,4 +3,5 @@ export enum ORDERS_SERVICES_NAMES {
   FIND_ALL_ORDERS = 'find_all_orders',
   FIND_ONE_ORDER = 'find_one_order',
   CHANGE_STATUS = 'change_order_status',
+  PAYMENT_SUCCEDED = 'payment.successful',
 }
